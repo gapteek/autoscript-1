4c3d02dc5c0c0a177121b818f94814f4b828f6a5
gapteek <nelayan.ikan7@gmail.com>
2014-03-04 01:51:22 +0800

    Update debian7.sh

--- debian7.sh
+++ debian7.sh
@@ -31,8 +31,7 @@ apt-get -y --purge remove bind9*;
 # update
 apt-get update; apt-get -y upgrade;
 
-# install webserver
-apt-get -y install nginx php5-fpm php5-cli
+
 
 # install essential package
 apt-get -y install bmon iftop htop nmap axel nano iptables traceroute sysv-rc-conf dnsutils bc nethogs openvpn vnstat less screen psmisc apt-file whois sslh ptunnel ngrep mtr git zsh mrtg snmp snmpd snmp-mibs-downloader unzip unrar rsyslog debsums rkhunter
@@ -57,52 +56,7 @@ chmod +x /usr/bin/screenfetch
 echo "clear" >> .profile
 echo "screenfetch" >> .profile
 
-# install webserver
-cd
-rm /etc/nginx/sites-enabled/default
-rm /etc/nginx/sites-available/default
-wget -O /etc/nginx/nginx.conf "https://raw.github.com/gapteek/autoscript-1/master/conf/nginx.conf"
-mkdir -p /home/vps/public_html
-echo "<pre>Setup by Gapteek | computika.org | arun ganteng | 7946F434</pre>" > /home/vps/public_html/index.html
-echo "<?php phpinfo(); ?>" > /home/vps/public_html/info.php
-wget -O /etc/nginx/conf.d/vps.conf "https://raw.github.com/gapteek/autoscript-1/master/conf/vps.conf"
-sed -i 's/listen = \/var\/run\/php5-fpm.sock/listen = 127.0.0.1:9000/g' /etc/php5/fpm/pool.d/www.conf
-service php5-fpm restart
-service nginx restart
 
-# install openvpn
-wget -O /etc/openvpn/openvpn.tar "https://raw.github.com/gapteek/autoscript-1/master/conf/openvpn-debian.tar"
-cd /etc/openvpn/
-tar xf openvpn.tar
-wget -O /etc/openvpn/1194.conf "https://raw.github.com/gapteek/autoscript-1/master/conf/1194.conf"
-service openvpn restart
-sysctl -w net.ipv4.ip_forward=1
-sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g' /etc/sysctl.conf
-wget -O /etc/iptables.up.rules "https://raw.github.com/gapteek/autoscript-1/master/conf/iptables.up.rules"
-sed -i '$ i\iptables-restore < /etc/iptables.up.rules' /etc/rc.local
-MYIP=`curl -s ifconfig.me`;
-MYIP2="s/xxxxxxxxx/$MYIP/g";
-sed -i $MYIP2 /etc/iptables.up.rules;
-iptables-restore < /etc/iptables.up.rules
-service openvpn restart
-
-# configure openvpn client config
-cd /etc/openvpn/
-wget -O /etc/openvpn/1194-client.ovpn "https://raw.github.com/gapteek/autoscript-1/master/conf/1194-client.conf"
-sed -i $MYIP2 /etc/openvpn/1194-client.ovpn;
-PASS=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 15 | head -n 1`;
-useradd -M -s /bin/false KangArie
-echo "gapteek:$PASS" | chpasswd
-echo "gapteek" > pass.txt
-echo "$PASS" >> pass.txt
-tar cf client.tar 1194-client.ovpn pass.txt
-cp client.tar /home/vps/public_html/
-cd
-# install badvpn
-wget -O /usr/bin/badvpn-udpgw "https://raw.github.com/gapteek/autoscript-1/master/conf/badvpn-udpgw"
-sed -i '$ i\screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300' /etc/rc.local
-chmod +x /usr/bin/badvpn-udpgw
-screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300
 
 # install mrtg
 wget -O /etc/snmp/snmpd.conf "https://raw.github.com/gapteek/autoscript-1/master/conf/snmpd.conf"
@@ -190,10 +144,8 @@ chmod +x limit.sh
 
 # finalisasi
 chown -R www-data:www-data /home/vps/public_html
-service nginx start
 service php-fpm start
 service vnstat restart
-service openvpn restart
 service snmpd restart
 service ssh restart
 service dropbear restart
@@ -203,12 +155,11 @@ service webmin restart
 
 # info
 clear
-echo "computika.org | @gapteek | gapteek | 7946F434"
+echo "computika.org | arun | ganteng | :p"
 echo "==============================================="
 echo ""
 echo "Service"
 echo "-------"
-echo "OpenVPN  : TCP 1194 (client config : http://$MYIP/client.tar)"
 echo "OpenSSH  : 22, 143"
 echo "Dropbear : 109, 110, 443"
 echo "Squid    : 8080 (limit to IP SSH)"
