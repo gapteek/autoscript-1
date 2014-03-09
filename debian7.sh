4c973deb3e8b598bd249757cdb3bc9fad1c6eebe
gapteek <nelayan.ikan7@gmail.com>
2014-03-10 03:44:03 +0800

    Update debian7.sh

--- debian7.sh
+++ debian7.sh
@@ -31,8 +31,7 @@ apt-get -y --purge remove bind9*;
 # update
 apt-get update; apt-get -y upgrade;
 
-# install webserver
-apt-get -y install nginx php5-fpm php5-cli
+
 
 # install essential package
 apt-get -y install bmon iftop htop nmap axel nano iptables traceroute sysv-rc-conf dnsutils bc nethogs openvpn vnstat less screen psmisc apt-file whois sslh ptunnel ngrep mtr git zsh mrtg snmp snmpd snmp-mibs-downloader unzip unrar rsyslog debsums rkhunter
@@ -45,50 +44,13 @@ sysv-rc-conf exim4 off
 # update apt-file
 apt-file update
 
-# setting vnstat
-vnstat -u -i venet0
-service vnstat restart
-
-# install screenfetch
-cd
-wget https://github.com/KittyKatt/screenFetch/raw/master/screenfetch-dev
-mv screenfetch-dev /usr/bin/screenfetch
-chmod +x /usr/bin/screenfetch
-echo "clear" >> .profile
-echo "screenfetch" >> .profile
-
-# install webserver
-cd
-rm /etc/nginx/sites-enabled/default
-rm /etc/nginx/sites-available/default
-wget -O /etc/nginx/nginx.conf "https://raw.github.com/gapteek/autoscript-1/master/conf/nginx.conf"
-mkdir -p /home/vps/public_html
-echo "<pre>Setup by arun gapteek | tidak ada manusia | yang mulai dari atas  | akan tetapi semua dari bawah</pre>" > /home/vps/public_html/index.html
-echo "<?php phpinfo(); ?>" > /home/vps/public_html/info.php
-wget -O /etc/nginx/conf.d/vps.conf "https://raw.github.com/gapteek/autoscript-1/master/conf/vps.conf"
-sed -i 's/listen = \/var\/run\/php5-fpm.sock/listen = 127.0.0.1:9000/g' /etc/php5/fpm/pool.d/www.conf
-service php5-fpm restart
-service nginx restart
 
 
-# install mrtg
-wget -O /etc/snmp/snmpd.conf "https://raw.github.com/gapteek/autoscript-1/master/conf/snmpd.conf"
-wget -O /root/mrtg-mem.sh "https://raw.github.com/gapteek/autoscript-1/master/conf/mrtg-mem.sh"
-chmod +x /root/mrtg-mem.sh
-cd /etc/snmp/
-sed -i 's/TRAPDRUN=no/TRAPDRUN=yes/g' /etc/default/snmpd
-service snmpd restart
-snmpwalk -v 1 -c public localhost 1.3.6.1.4.1.2021.10.1.3.1
-mkdir -p /home/vps/public_html/mrtg
-cfgmaker --zero-speed 100000000 --global 'WorkDir: /home/vps/public_html/mrtg' --output /etc/mrtg.cfg public@localhost
-curl "https://raw.github.com/gapteek/autoscript-1/master/conf/mrtg.conf" >> /etc/mrtg.cfg
-sed -i 's/WorkDir: \/var\/www\/mrtg/# WorkDir: \/var\/www\/mrtg/g' /etc/mrtg.cfg
-sed -i 's/# Options\[_\]: growright, bits/Options\[_\]: growright/g' /etc/mrtg.cfg
-indexmaker --output=/home/vps/public_html/mrtg/index.html /etc/mrtg.cfg
-if [ -x /usr/bin/mrtg ] && [ -r /etc/mrtg.cfg ]; then mkdir -p /var/log/mrtg ; env LANG=C /usr/bin/mrtg /etc/mrtg.cfg 2>&1 | tee -a /var/log/mrtg/mrtg.log ; fi
-if [ -x /usr/bin/mrtg ] && [ -r /etc/mrtg.cfg ]; then mkdir -p /var/log/mrtg ; env LANG=C /usr/bin/mrtg /etc/mrtg.cfg 2>&1 | tee -a /var/log/mrtg/mrtg.log ; fi
-if [ -x /usr/bin/mrtg ] && [ -r /etc/mrtg.cfg ]; then mkdir -p /var/log/mrtg ; env LANG=C /usr/bin/mrtg /etc/mrtg.cfg 2>&1 | tee -a /var/log/mrtg/mrtg.log ; fi
-cd
+
+
+
+
+
 
 # setting port ssh
 sed -i '/Port 22/a Port 143' /etc/ssh/sshd_config
@@ -104,28 +66,11 @@ echo "/bin/false" >> /etc/shells
 service ssh restart
 service dropbear restart
 
-# install vnstat gui
-cd /home/vps/public_html/
-wget http://www.sqweek.com/sqweek/files/vnstat_php_frontend-1.5.1.tar.gz
-tar xf vnstat_php_frontend-1.5.1.tar.gz
-rm vnstat_php_frontend-1.5.1.tar.gz
-mv vnstat_php_frontend-1.5.1 vnstat
-cd vnstat
-sed -i 's/eth0/venet0/g' config.php
-sed -i "s/\$iface_list = array('venet0', 'sixxs');/\$iface_list = array('venet0');/g" config.php
-sed -i "s/\$language = 'nl';/\$language = 'en';/g" config.php
-sed -i 's/Internal/Internet/g' config.php
-sed -i '/SixXS IPv6/d' config.php
-cd
+
 
 # install fail2ban
 apt-get -y install fail2ban;service fail2ban restart
 
-# install squid
-apt-get -y install squid
-wget -O /etc/squid/squid.conf "https://raw.github.com/gapteek/autoscript-1/master/conf/squid.conf"
-sed -i $MYIP2 /etc/squid/squid.conf;
-service squid restart
 
 # install webmin
 cd
@@ -156,15 +101,10 @@ chmod +x user-limit.sh
 chmod +x limit.sh
 
 # finalisasi
-chown -R www-data:www-data /home/vps/public_html
-service nginx start
-service php-fpm start
-service vnstat restart
-service snmpd restart
+
 service ssh restart
 service dropbear restart
 service fail2ban restart
-service squid restart
 service webmin restart
 
 # info
